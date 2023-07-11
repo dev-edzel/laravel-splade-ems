5f826b07f3634cda0b06f8af564502b70595c91c
Edzel <dev.edzeldacanay@gmail.com>
2023-07-11 14:17:38 +0800

    modify tables

--- database/migrations/2023_07_11_060159_create_countries_table.php
+++ database/migrations/2023_07_11_060159_create_countries_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
+            $table->char('country_code', 3);
+            $table->string('name', 60);
             $table->timestamps();
         });
     }
